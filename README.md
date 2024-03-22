184d4dbd4c27b3be922180f1ceb60ad7ac198672
King555 <king.mulo@gmail.com>
2024-03-21 18:29:50 -0700

    Added output to table and annotation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# merge
+
+Especially for Anime lovers, and those who need to attach an external audio track to serials.
+A program was written to merge audio tracks into an MKV container.
+
+Runs on Linux from the console.
+Place the program in the directory with the video and audio files, run it, and give answers to the questions while starting.
+
+MKVmerge must be installed on the system
--- merge.py
+++ merge.py
@@ -4,12 +4,17 @@ import os
 import subprocess
 import re
 
+from rich.progress import Progress
+from rich.console import Console
+from rich.table import Table
+
 directory_path = '.'
-
 file_dict = {}
+output_path = input("Specify the path to save the results: ")
+season = input("Season? : ")
+pattern = r'\b(\d{2})\b'   #If the series has 3 digits, then change the constant
 
-pattern = r'\b(\d{2})\b'
-
+#Search for files with the same number
 for filename in os.listdir(directory_path):
     if os.path.isfile(os.path.join(directory_path, filename)):
         match = re.search(pattern, filename)
@@ -17,26 +22,32 @@ for filename in os.listdir(directory_path):
             number = match.group(1)
             file_dict.setdefault(number, []).append(filename)
 
-output_path = input("Specify the path to save the results: ")
-
-season = input("Season? : ")
-
+#Checking and creating an output folder
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
-for number, filenames in file_dict.items():
-    if len(filenames) > 1:
-        files_to_merge = [os.path.join(directory_path, filename) for filename in filenames]
+#Beautiful result output
+table = Table(title="Result")
+table.add_column("No.", style="cyan", no_wrap=True)
+table.add_column("File Name", style="magenta")
+table.add_column("Status", justify="center", style="green")
 
-        output_file = os.path.join(output_path, f'output_S0{season}E{number}.mkv')
+#Main program
+with Progress() as progress:                                                    
+    task_id = progress.add_task("[green]Processing...", total=len(file_dict))   
 
-        cmd = ['mkvmerge', '-o', output_file] + files_to_merge
+    for number, filenames in file_dict.items():
+        if len(filenames) > 1:
+            files_to_merge = [os.path.join(directory_path, filename) for filename in filenames]
+            output_file = os.path.join(output_path, f'output_S0{season}E{number}.mkv')
+            cmd = ['mkvmerge', '-q', '-o', output_file] + files_to_merge
+            subprocess.run(cmd)
 
-        subprocess.run(cmd)
+            table.add_row(f'{number}', f'{output_file}', "✅")
+            progress.advance(task_id)
 
-        print(f"File {output_file} created with number {number} from the next files:")
-        for filename in filenames:
-            print(filename)
+console = Console()
+console.print(table)
 
-subprocess.run("chown", "-R", "plex:plex", output_path)
+subprocess.run(["chown", "-R", "plex:plex", output_path])
 print("Directory owner has been changed to PLEX")
